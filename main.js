783e0d63f9fd5ecaaa7d79c4b226fdbcb075985a
Farghana Hussain <farghana@gmail.com>
2024-02-15 13:27:08 -0500

    Refactor rewind function to include forward video function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const playPauseBtn = document.querySelector("#play-pause-button");
 const video = document.querySelector("video");
 const rewindBtn = document.querySelector("#rewind-button");
+const forwardBtn = document.querySelector("#forward-button");
 
 const playPauseVideo = () => {
 	//use the paused method available in the Video API 
@@ -17,10 +18,11 @@ const updatePlayPauseIcon = ()=>{
   icon.textContent = video.paused ? "play_arrow" : "paused";
 }
 
-const rewindVideo = () => {
-  video.currentTime += -10;
+const rewindForwardVideo = (type) => {
+  video.currentTime += type === "rewind" ? -10 : 10;
 }
 
 playPauseBtn.addEventListener("click", playPauseVideo);
 video.addEventListener("click", playPauseVideo);
-rewindBtn.addEventListener("click", rewindVideo);
+rewindBtn.addEventListener("click", () => rewindForwardVideo('rewind'));
+forwardBtn.addEventListener("click", () => rewindForwardVideo('forward'));
