a828d88e56127fc114ea4f2ef1775d0a9868702d
Farghana Hussain <farghana@gmail.com>
2024-02-15 14:01:11 -0500

    Add mute/unmute function to video player

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const playPauseBtn = document.querySelector("#play-pause-button");
 const video = document.querySelector("video");
 const rewindBtn = document.querySelector("#rewind-button");
 const forwardBtn = document.querySelector("#forward-button");
+const volumeBtn = document.querySelector("#volume-button");
 
 const playPauseVideo = () => {
 	//use the paused method available in the Video API 
@@ -22,7 +23,18 @@ const rewindForwardVideo = (type) => {
   video.currentTime += type === "rewind" ? -10 : 10;
 }
 
+const controlVolume = () => {
+  video.muted = video.muted ? false : true;
+  updateVolumeIcon();
+}
+const updateVolumeIcon = ()=>{
+  const icon = volumeBtn.querySelector("i");
+  icon.textContent = "";
+  icon.textContent = video.muted ? "volume_off" : "volume_up";
+}
+
 playPauseBtn.addEventListener("click", playPauseVideo);
 video.addEventListener("click", playPauseVideo);
 rewindBtn.addEventListener("click", () => rewindForwardVideo('rewind'));
 forwardBtn.addEventListener("click", () => rewindForwardVideo('forward'));
+volumeBtn.addEventListener("click", controlVolume);
